30513545ea88be1e60da132e00c6c64aa1d63c77
faykris <faykris28@gmail.com>
2021-09-25 12:43:49 -0500

    add task 2 files - waiting for Manual Review

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
+    let i = 0;
+    let a_list = [];
     $('input[type=checkbox]').on('change', function() {
         if ($(this).is(':checked') ) {
-            console.log("Checkbox " + $(this).prop("id") +  " (" + $(this).val() + ") => Seleccionado" );
+            i++;
+            a_list.push({key:$(this).prop("id"), value:$(this).prop("name")});
         } else {
-            console.log("Checkbox " + $(this).prop("id") +  " (" + $(this).val() + ") => Deseleccionado");
+            for (let j = 0; j < a_list.length; j++) {
+                if (a_list[j].key === $(this).prop("id")) {
+                    a_list.splice(j, 1)
+                }
+            }
+            i--;
+        }
+        if (i === 0) {
+            $('.amenities_text').html("&nbsp;");
+        }
+        else {
+            let a_string = "";
+            for (let j = 0; j < a_list.length; j++)
+            {
+                if (j > 0) {
+                    a_string += ", "
+                }
+                a_string += a_list[j].value;
+            }
+            $('.amenities_text').text(a_string);
         }
     });
 });
